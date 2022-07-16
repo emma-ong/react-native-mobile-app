5827a203af5a3f1f2f9050c0a1946ffb35b3393f
emma-ong <emmaong.at@gmail.com>
2022-07-16 23:40:22 +1200

    Swap cattery image for shelter

--- App.js
+++ App.js
@@ -23,12 +23,14 @@ import { StatusBar } from "expo-status-bar"
 import { View, Text, StyleSheet } from "react-native"
 import Header from "./src/components/Header"
 import Search from "./src/components/Search"
+import CategoryItem from "./src/components/CategoryItem"
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
       <Search />
+      <CategoryItem />
       <StatusBar />
     </View>
   )
---
+++ src/components/CategoryItem.js
@@ -0,0 +1,9 @@
+import { Text, View, StyleSheet } from "react-native" 
+
+export default function CategoryItem() {
+  return(
+    <View>
+      <Text>Hello</Text>
+    </View>
+  )
+}
